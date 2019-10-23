179698859d5f1b95e26696833f2621b267dc8b55
Logan Smith <logsmith06@gmail.com>
2019-10-23 22:11:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,11 @@ text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
