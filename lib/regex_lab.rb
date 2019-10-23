630425cc8a5fe692d10c826b73a6773a88ff7c91
Logan Smith <logsmith06@gmail.com>
2019-10-23 22:04:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,11 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+if text.start_with?(/[un]/) && text.end_with?(/[ing]/)
+  true
+else
+  false
+end
 end
 
 def words_five_letters_long(text)
